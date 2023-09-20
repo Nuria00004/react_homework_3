a5b10e8852294be4df60cb3635062760c8d70669
Nuria00004 <nuriarustambekova4@gmail.com>
2023-09-20 13:30:55 +0600

    hw3

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  const handleInputChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Список задач</h1>
+      <input
+        type="text"
+        placeholder="Добавить задачу"
+        value={task}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddTask}>Добавить</button>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
